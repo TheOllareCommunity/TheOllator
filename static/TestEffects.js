0240e1b5b56a174788ed1d37ab022602b5a19346
GabrieleMarelli <57753418+GabrieleMarelli@users.noreply.github.com>
2021-05-04 16:12:24 +0200

    effects

--- static/TestEffects.js
+++ static/TestEffects.js
@@ -5,13 +5,14 @@ const player = new Tone.Player({
 	autostart: true,
 })
 
-let minV=-160, maxV=160;
-let x = 10; 
-//const gainNode = new Tone.Gain(0.1);
-const gainNode = new Tone.Gain((maxV-minV)/(x-minV)*0.1);  //divisione per 0??
-//PUMP
+let minV=0, maxV=360;
+let x = 100; 
+const gainNode = new Tone.Gain(0.5);
+//DISTORTION
 const distortion = new Tone.Distortion((x-minV)/(maxV-minV));//.toDestination(); //distortion range 0-1
+//COMPRESSOR
 const compression = new Tone.Compressor(-30, ((x-minV)/(maxV-minV)*(20-1)+1));//.toDestination(); //compression range 1-20
+//EQUALIZER
 const eq = new Tone.EQ3((x-minV)/(maxV-minV)*20, (x-minV)/(maxV-minV)*(-15), (x-minV)/(maxV-minV)*10 );//.toDestination();
 //BITCRUSHER
 const bit = new Tone.BitCrusher((x-minV)/(maxV-minV)*(8-1)+1);//.toDestination();  //bitcrusher range 1-8
@@ -19,11 +20,13 @@ const bit = new Tone.BitCrusher((x-minV)/(maxV-minV)*(8-1)+1);//.toDestination()
 const trem = new Tone.Tremolo(10, (x-minV)/(maxV-minV)).toMaster().start();  //depth range 0-1
 //CHORUS
 const chorus = new Tone.Chorus(4, (x-minV)/(maxV-minV)*(100-1)+1, (x-minV)/(maxV-minV));//.toDestination(); //time delay range 1-100 ms, depth 0-1
+//CHEBYSHEV
 const cheby = new Tone.Chebyshev((x-minV)/(maxV-minV)*(100-1)+1);//.toDestination(); //range 1-100
+//PITCH SHIFT
+const ps = new Tone.PitchShift((x-minV)/(maxV-minV)*(48+48)-48); //range +48/-48 semitones (8 ottave)
 
 //connect in parallel
 //player.connect(gainNode);
-
 //player.connect(distortion);
 //player.connect(compression);
 //player.connect(eq);
@@ -31,29 +34,41 @@ const cheby = new Tone.Chebyshev((x-minV)/(maxV-minV)*(100-1)+1);//.toDestinatio
 //player.connect(trem);
 //player.connect(chorus);
 //player.connect(cheby);
+//player.connect(ps);
+
 //connect in series
-player.chain(gainNode,distortion,compression,eq,Tone.Destination);
+player.chain(gainNode,distortion,Tone.Destination);
+player.chain(gainNode,compression,Tone.Destination);
+player.chain(gainNode,eq,Tone.Destination);
+player.chain(gainNode,distortion,eq,compression,Tone.Destination); //PUMP
 player.chain(gainNode,bit,Tone.Destination);
 player.chain(gainNode,trem,Tone.Destination);
-player.chain(gainNode,chorus,cheby,Tone.Destination);
+player.chain(gainNode,chorus,Tone.Destination);
+player.chain(gainNode,cheby,Tone.Destination);
+player.chain(gainNode,ps,Tone.Destination);
 
 
 
 //PAD
-let mouseX=10, mouseY=50;  //range 0-100 posizione del mouse sul pad in x e y 
-//1
-const reverb = new Tone.Reverb(mouseX).toDestination();
-const delay = new Tone.Delay(mouseY).toDestination();
-//player.connect(reverb);
-//player.connect(delay);
-player.chain(gainNode,reverb,delay,Tone.Destination);
-//2
-const freeverb = new Tone.Freeverb((mouseX-minV)/(maxV-minV), (mouseY-minV)/(maxV-minV)*(5000-20)+20);//.toDestination();
+let mouseX=20, mouseY=80;
+
+//reverbs
+const freeverb = new Tone.Freeverb((mouseX-minV)/(maxV-minV), (mouseY-minV)/(maxV-minV)*(20000-20)+20);//.toDestination();
+const reverb = new Tone.Reverb((mouseY-minV)/(maxV-minV)*4);//.toDestination();
+const jcrReverb = new Tone.JCReverb((mouseY-minV)/(maxV-minV));//.toDestination();
 //player.connect(freeverb);
 player.chain(gainNode,freeverb,Tone.Destination);
-//3
-const jcrReverb = new Tone.JCReverb((mouseX-minV)/(maxV-minV));//.toDestination();
-const feedbackDelay = new Tone.FeedbackDelay(50, (mouseY-minV)/(maxV-minV));//.toDestination();
-//player.connect(jcrReverb);
+//player.chain(gainNode,reverb,Tone.Destination);
+//player.chain(gainNode,jcrReverb,Tone.Destination); 
+
+//delays
+const feedbackDelay = new Tone.FeedbackDelay((mouseX-minV)/(maxV-minV)*(3-0.1)+0.1, (mouseY-minV)/(maxV-minV));//.toDestination();
+const pingPong = new Tone.PingPongDelay((mouseX-minV)/(maxV-minV)*(3-0.1)+0.1, (mouseY-minV)/(maxV-minV));
+const delay = new Tone.Delay((mouseX-minV)/(maxV-minV)*4);//.toDestination();
 //player.connect(feedbackDelay);
-player.chain(gainNode,feedbackDelay,jcrReverb,Tone.Destination); 
+player.chain(gainNode,feedbackDelay,Tone.Destination); 
+player.chain(gainNode,pingPong,Tone.Destination);
+//player.chain(gainNode,delay,Tone.Destination);
+
+player.chain(gainNode,delay,jcrReverb,Tone.Destination); 
+player.chain(gainNode,delay,reverb,Tone.Destination);
