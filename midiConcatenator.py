6d8f4dbdb9bce340893cbff7f0abe7ccb55f11f2
PilvioSol <polsilvio@gmail.com>
2021-05-27 16:10:37 +0200

    Update midiConcatenator.py

--- midiConcatenator.py
+++ midiConcatenator.py
@@ -4,19 +4,12 @@ file1 = "/Users/PilvioSol/Desktop/TheOllator/static/MIDI_2bar/BeatA_drum_2bar.mi
 file2 = "/Users/PilvioSol/Desktop/TheOllator/static/MIDI_2bar/BeatB_drum_2bar.mid"
 
 pattern1 = midi.read_midifile(file1)
-#print(pattern1)
+
 pattern2 = midi.read_midifile(file2)
 
 tracker = midi.Track()
 pattern = midi.Pattern()
-pattern.resolution=96
-
-
-''''for track in pattern1:
-    tracker.append(track)
-
-for track in pattern2:
-    tracker.append(track)'''
+pattern.resolution = 96
 
 for track in pattern1:
     for event in track[:-1]:
@@ -26,7 +19,6 @@ for track in pattern2:
     for event in track[3:-1]:
         tracker.append(event)
 
-#print(tracker)
 pattern.append(tracker)
 print(pattern)
 
